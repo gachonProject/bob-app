874fb55c26ffa66b57a7a0109cf384e28a75dbf0
97baek <170410_@naver.com>
2021-05-09 00:13:57 +0900

    [ADD] 채팅 리스트 가져오기 로직 추가

--- src/pages/ChatListPage/index.js
+++ src/pages/ChatListPage/index.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getConversationList } from "../../actions/user.actions";
 import Layout from "../../components/Layout";
 
 const ChatListPage = () => {
+  const currentUser = useSelector((state) => state.auth);
+  const conversationList = useSelector((state) => state.user.conversationList);
+  const dispatch = useDispatch();
+  // console.log(currentUser);
+  // const getConversationList = async () => {
+  //   const db = await firestore;
+  //   if (currentUser.uid) {
+  //     await db
+  //       .collection("conversations")
+  //       .where("user_uid_1", "==", currentUser.uid)
+  //       .orderBy("createdAt", "desc")
+  //       .onSnapshot((querySnpshot) => {
+  //         const conversations = [];
+  //         querySnpshot.forEach((doc) => {
+  //           if (!conversations.includes(doc.data().user_uid_2)) {
+  //             conversations.push(doc.data());
+  //             conversations.push(doc.data().user_uid_2);
+  //           }
+  //           console.log(doc.data());
+  //         });
+  //         console.log(conversations);
+  //       });
+  //   }
+  // };
+
+  useEffect(() => {
+    dispatch(getConversationList(currentUser.uid));
+  }, []);
+  console.log(conversationList);
+
   return (
     <Layout title={"채팅목록"}>
       <h1>채팅목록</h1>
