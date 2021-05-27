07d9148bced818f561c94acbffbebdec132b1145
97baek <170410_@naver.com>
2021-05-27 22:59:30 +0900

    [UPDATE] 회원탈퇴 시 작성 게시글, 댓글, 채팅 삭제

--- src/pages/DeleteUserPage/index.js
+++ src/pages/DeleteUserPage/index.js
@@ -5,6 +5,7 @@ import { Redirect } from "react-router";
 import { Container } from "./styles";
 import { Buttons } from "../ProfilePage/styles";
 import Layout from "../../components/Layout";
+import { firestore } from "../../fbase";
 
 const DeleteUserPage = ({ history }) => {
   const dispatch = useDispatch();
@@ -16,9 +17,117 @@ const DeleteUserPage = ({ history }) => {
     }
   }, [auth]);
 
+  let boardid = [];
+  let commentid = [];
+  let convid1 = [];
+  let convid2 = [];
+
+  useEffect(() => {
+    if (!auth.authenticated) {
+      return <Redirect to="/login" />;
+    }
+  }, [auth]);
+
+  firestore
+    .collection("board")
+    .where("owner", "==", auth.uid)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        boardid.push(doc.id);
+      });
+    })
+    .catch((error) => {});
+
+  firestore
+    .collection("comments")
+    .where("owner", "==", auth.uid)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        commentid.push(doc.id);
+      });
+    })
+    .catch((error) => {});
+
+  firestore
+    .collection("conversations")
+    .where("user_uid_1", "==", auth.uid)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        convid1.push(doc.id);
+      });
+    })
+    .catch((error) => {});
+
+  firestore
+    .collection("conversations")
+    .where("user_uid_2", "==", auth.uid)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        convid2.push(doc.id);
+      });
+    })
+    .catch((error) => {});
+
   const onDeleteUser = () => {
     dispatch(deleteAccount(auth.uid));
-    // history.push("/login");
+    boardid.map((board) => {
+      firestore
+        .collection("board")
+        .doc(board)
+        .delete()
+        .then(() => {})
+        .catch((error) => {});
+      const comid = [];
+      firestore
+        .collection("comments")
+        .where("boardId", "==", board)
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            comid.push(doc.id);
+          });
+        })
+        .catch((error) => {});
+      comid.map((com) => {
+        firestore
+          .collection("comments")
+          .doc(com)
+          .delete()
+          .then(() => {})
+          .catch((error) => {});
+      });
+    });
+
+    commentid.map((comment) => {
+      firestore
+        .collection("comments")
+        .doc(comment)
+        .delete()
+        .then(() => {})
+        .catch((error) => {});
+    });
+
+    convid1.map((con1) => {
+      firestore
+        .collection("conversations")
+        .doc(con1)
+        .delete()
+        .then(() => {})
+        .catch((error) => {});
+    });
+
+    convid2.map((con2) => {
+      firestore
+        .collection("conversations")
+        .doc(con2)
+        .delete()
+        .then(() => {})
+        .catch((error) => {});
+    });
   };
 
   return (
